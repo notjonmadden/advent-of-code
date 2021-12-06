034ffecab8fee69e2f80a7972750cddcc2eb7551
Jon Madden <notjonmadden@rocketinsights.com>
2021-12-06 12:31:32 -0500

    File.read! -> File.stream!

--- day_1/lib/day1.ex
+++ day_1/lib/day1.ex
@@ -5,14 +5,14 @@ defmodule Day1.Problem do
 
   def main(_args) do
     result =
-      File.read!("input.txt")
-      |> String.split("\n")
-      |> Enum.drop(-1)
-      |> Enum.map(&String.to_integer/1)
+      File.stream!("input.txt")
+      |> Enum.map(&parse_integer/1)
       |> Enum.reduce(&count_depth_increases/2)
 
     IO.puts("#{result.total} increases in the input file")
   end
+
+  def parse_integer(input_line), do: input_line |> String.trim() |> String.to_integer()
 
   defp count_depth_increases(depth, acc) when is_number(acc),
     do: %Accumulator{total: 0, last: depth}
